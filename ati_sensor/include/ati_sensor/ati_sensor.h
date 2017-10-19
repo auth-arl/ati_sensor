20adf1c288073284d026c55a4535495bfa85b5ef
thprapav <thprapav@gmail.com>
2017-10-19 14:56:25 +0300

    Add sensor wrapper

---
+++ ati_sensor/include/ati_sensor/ati_sensor.h
@@ -0,0 +1,47 @@
+/**
+ * Copyright (C) 2016 AUTH-ARL
+ */
+
+#ifndef ATI_SENSOR_ATI_SENSOR_H
+#define ATI_SENSOR_ATI_SENSOR_H
+
+#include <autharl_core/robot/sensor.h>
+#include <ati_sensor/ft_sensor.h>
+#include <ros/ros.h>
+#include <memory>
+#include <vector>
+#include <string>
+
+
+namespace ati
+{
+namespace sensor
+{
+class Sensor : public arl::robot::Sensor
+{
+public:
+  explicit Sensor(const std::string& name = "Ati Sensor");
+
+  void getData(KDL::JntArray &output);
+  void getData(arma::vec &output);
+  void getData(Eigen::VectorXd &output);
+  template <typename T> void getDataTemplate(T &output)
+  {
+    static float temp[6];
+    output.resize(6);
+    sensor_->getMeasurements(temp);  // ,rdt,ft);
+    for (size_t i = 0; i < 6; i++) {
+      output(i) = temp[i];
+    }
+  }
+
+  bool isOk();
+
+private:
+  std::shared_ptr<ati::FTSensor> sensor_;
+
+};
+}  // namespace sensor
+}  // namespace api
+
+#endif  // ATI_SENSOR_ATI_SENSOR_H
---
+++ ati_sensor/src/ati_sensor.cpp
@@ -0,0 +1,50 @@
+/**
+ * Copyright (C) 2016 AUTH-ARL
+ */
+
+#include <ati_sensor/ati_sensor.h>
+#include <iostream>
+#include <string>
+
+#define JOINT_SIZE 7
+
+namespace ati
+{
+  namespace sensor
+  {
+    Sensor::Sensor(const std::string& name) :
+        arl::robot::Sensor(name)
+    {
+      std::cout << "Creating sensor" << std::endl;
+//      sensor_ = std::make_shared<ati::FTSensor>();
+      sensor_.reset(new ati::FTSensor());
+
+      std::cout << "Setting timeout to 1.0 sec" << std::endl;
+      sensor_->setTimeout(1.0);
+
+      std::cout << "Initializing sensor" << std::endl;
+      sensor_->init("192.168.2.1");
+
+      std::cout << "Setting sensor bias" << std::endl;
+      sensor_->setBias();
+    }
+
+    void Sensor::getData(KDL::JntArray &output)
+    {
+      getDataTemplate(output);
+    }
+    void Sensor::getData(arma::vec &output)
+    {
+      getDataTemplate(output);
+    }
+    void Sensor::getData(Eigen::VectorXd &output)
+    {
+      getDataTemplate(output);
+    }
+
+    bool Sensor::isOk()
+    {
+      return true;
+    }
+  }  // namespace sensor
+}  // namespace ati
